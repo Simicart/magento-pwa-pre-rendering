05aa58615171bf58494e4caf4eb4c155c2e710e6
peter <peter@simicart.com>
2019-04-04 16:37:54 +0700

    update cart + contact

---
+++ pages/Tapita/Cart.js
@@ -0,0 +1,8 @@
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 4/4/19
+ * Time: 4:33 PM
+ */
+import Cart from '../../src/App/Tapita/Cart'
+export default Cart
---
+++ pages/Tapita/Contact.js
@@ -0,0 +1,8 @@
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 4/4/19
+ * Time: 4:34 PM
+ */
+import Contact from '../../src/App/Tapita/Contact'
+export default Contact
