8779d2cd93f0a16536451aee2a104f5f5722aea5
peter <peter@simicart.com>
2019-04-08 16:18:24 +0700

    fix name folder

--- router/Tapita/Config.js
+++ router/Tapita/Config.js
@@ -1,52 +1,52 @@
 const tapitaRoute = {
     home : {
         name : 'home',
-        page : 'Tapita1/Home',
+        page : 'Tapita/Home',
         pattern : '/'
     },
     product_detail : {
         name : 'product_detail',
-        page : 'Tapita1/Product',
+        page : 'Tapita/Product',
         pattern : '/product/:id?'
     },
     product_list : {
         name : 'product_list',
-        page : 'Tapita1/Products',
+        page : 'Tapita/Products',
         pattern : '/products'
     },
     cms : {
         name : 'cms',
-        page : 'Tapita1/Cms',
+        page : 'Tapita/Cms',
         pattern : '/cms/:id?'
     },
     cart : {
         name : 'cart',
-        page : 'Tapita1/Cart',
+        page : 'Tapita/Cart',
         pattern : '/checkout/cart'
     },
     checkout : {
         name : 'checkout',
-        page : 'Tapita1/Checkout',
+        page : 'Tapita/Checkout',
         pattern : '/checkout/onepage'
     },
     customer_login :{
         name : 'customer_login',
-        page : 'Tapita1/Login',
+        page : 'Tapita/Login',
         pattern : '/customer/account/login'
     },
     contact :{
         name : 'contact',
-        page : 'Tapita1/Contact',
+        page : 'Tapita/Contact',
         pattern : '/contacts'
     },
     customer_dashboard : {
         name : 'customer_dashboard',
-        page : 'Tapita1/CustomerDashboard',
+        page : 'Tapita/CustomerDashboard',
         pattern : '/customer'
     },
     url_match : {
         name : 'url_match',
-        page : 'Tapita1/UrlMatch',
+        page : 'Tapita/UrlMatch',
         pattern : '/(.*)'
     }
 }
